ad972ef4eed39f83c53996c8b2351ea38936e7bb
Hari Rijal <reezalharee2015@gmail.com>
2021-02-23 16:02:04 +0545

    Fix: changes

--- wp-content/themes/theRevisedCafe/functions.php
+++ wp-content/themes/theRevisedCafe/functions.php
@@ -221,3 +221,84 @@ $args = array(
  
 register_post_type( 'news',$args);
 }
+
+/**
+ * Register a custom post type called "foodmenu".
+ *
+ * @see get_post_type_labels() for label keys.
+ */
+function wpdocs_codex_foodmenu_init() {
+    $labels = array(
+        'name'                  => _x( 'FoodMenu', 'Post type general name', 'textdomain' ),
+        'singular_name'         => _x( 'FoodMenu', 'Post type singular name', 'textdomain' ),
+        'menu_name'             => _x( 'FoodMenu', 'Admin Menu text', 'textdomain' ),
+        'name_admin_bar'        => _x( 'FoodMenu', 'Add New on Toolbar', 'textdomain' ),
+        'add_new'               => __( 'Add New', 'textdomain' ),
+        'add_new_item'          => __( 'Add New FoodMenu', 'textdomain' ),
+        'new_item'              => __( 'New FoodMenu', 'textdomain' ),
+        'edit_item'             => __( 'Edit FoodMenu', 'textdomain' ),
+        'view_item'             => __( 'View FoodMenu', 'textdomain' ),
+        'all_items'             => __( 'All FoodMenus', 'textdomain' ),
+        'search_items'          => __( 'Search FoodMenus', 'textdomain' ),
+        'parent_item_colon'     => __( 'Parent FoodMenus:', 'textdomain' ),
+        'not_found'             => __( 'No foodmenus found.', 'textdomain' ),
+        'not_found_in_trash'    => __( 'No foodmenus found in Trash.', 'textdomain' ),
+        'featured_image'        => _x( 'FoodMenu Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'textdomain' ),
+        'set_featured_image'    => _x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+        'remove_featured_image' => _x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+        'use_featured_image'    => _x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+        'archives'              => _x( 'FoodMenu archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'textdomain' ),
+        'insert_into_item'      => _x( 'Insert into foodmenu', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'textdomain' ),
+        'uploaded_to_this_item' => _x( 'Uploaded to this foodmenu', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'textdomain' ),
+        'filter_items_list'     => _x( 'Filter foodmenus list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'textdomain' ),
+        'items_list_navigation' => _x( 'FoodMenus list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'textdomain' ),
+        'items_list'            => _x( 'FoodMenus list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'textdomain' ),
+    );
+ 
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'foodmenu' ),
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => false,
+        'menu_position'      => null,
+        'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
+    );
+ 
+    register_post_type( 'foodmenu', $args );
+}
+ 
+add_action( 'init', 'wpdocs_codex_foodmenu_init' );
+
+    $labels2 = array(
+        'name'                       => _x( 'Meal Type', 'Taxonomy General Name', 'text_domain' ),
+        'singular_name'              => _x( 'Meal Type', 'Taxonomy Singular Name', 'text_domain' ),
+        'menu_name'                  => __( 'Meal Type', 'text_domain' ),
+        'all_items'                  => __( 'All meals', 'text_domain' ),
+        'parent_item'                => __( 'Parent Item', 'text_domain' ),
+        'parent_item_colon'          => __( 'Parent Item:', 'text_domain' ),
+        'new_item_name'              => __( 'New Item Name', 'text_domain' ),
+        'add_new_item'               => __( 'Add New Meal Type', 'text_domain' ),
+        'edit_item'                  => __( 'Edit Meal Type', 'text_domain' ),
+        'update_item'                => __( 'Update Item', 'text_domain' ),
+        'separate_items_with_commas' => __( 'Separate items with commas', 'text_domain' ),
+        'search_items'               => __( 'Search Items', 'text_domain' ),
+        'add_or_remove_items'        => __( 'Add or remove items', 'text_domain' ),
+        'choose_from_most_used'      => __( 'Choose from the most used items', 'text_domain' ),
+        'not_found'                  => __( 'Not Found', 'text_domain' ),
+    );
+    $args2 = array(
+        'labels'                     => $labels2,
+        'hierarchical'               => true,
+        'public'                     => true,
+        'show_ui'                    => true,
+        'show_admin_column'          => true,
+        'show_in_nav_menus'          => true,
+        'show_tagcloud'              => true,
+    );
+    register_taxonomy( 'mealtype', 'foodmenu', $args2 );
